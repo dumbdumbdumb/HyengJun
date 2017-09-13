94ec80fb84fdd8534d9e28972e29e88166fae862
YoungEuny <tjduddmd1287@naver.com>
2017-09-13 17:04:10 +0900

    report card: Now time

--- report_card.c
+++ report_card.c
@@ -6,15 +6,13 @@ int main()
 
 	printf("9/13 -1");
 	printf("3th commit");
-<<<<<<< HEAD
 	printf("Hello");
 	
-=======
 	printf("jun");
->>>>>>> 19ebd77b2f145b834faf24fd814f7d52d0fd3be5
 	printf("hyeung");
 	printf("yang");
 	printf("test");
 	printf("Let's go");
+	printf("now 5:03");
 	return 0;
 }
